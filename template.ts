914d77c7871047580cb0bdf7e80651743fabca33
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-11-03 17:48:08 +0000

    Make the template more generic

--- template.ts
+++ template.ts
@@ -10,7 +10,7 @@ export const blogPost_000001 = new BlogPost(
     'Title',
     'Dave Hudson',
     '2024-10-21',
-    '/blog/000001-Founding-M6R',
+    '/blog/000001-Blog-post',
     'Summary.',
     null,
     blogArticle_000001
